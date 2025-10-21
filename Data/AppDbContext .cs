7ba02ced818352f8f47a6567bd1c1717329e48a7
sajidaperveen1024 <sajidaperveen1024@gmail.com>
2025-10-21 23:40:39 +0100

    Initial commit - Azure SQL Employee Manager project

---
+++ Data/AppDbContext .cs
@@ -0,0 +1,12 @@
+﻿using EmployeeManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManager.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Employee> Employees => Set<Employee>();
+    }
+    
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeManager.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Position { get; set; } = "";
+        public decimal Salary { get; set; }
+    }
+}
